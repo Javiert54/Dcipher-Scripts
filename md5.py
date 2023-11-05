e511f97a5926f6c7ea9a82ad50188194cde3212d
Javiert54 <jdiazmach@gmail.com>
2023-11-05 14:23:08 +0000

    update

--- md5.py
+++ md5.py
@@ -1,5 +1,20 @@
-#Necesitas tener rockyou.txt en el mismo directorio que md5.py
-with open('nombre_del_archivo.txt', 'r') as archivo:
-    lista = [linea.strip() for linea in archivo]
+import hashlib
 
-print(lista)
+#Necesitas tener rockyou.txt en el mismo directorio que md5.py
+with open('Dcipher-Scripts/rockyou.txt', 'r', errors='ignore') as archivo:
+    passwordsList = [linea.strip() for linea in archivo]
+
+#Para mostrar la lista del blog de notas
+print(len(passwordsList))
+
+def hash_md5(cadena):
+    resultado = hashlib.md5(cadena.encode())
+    return resultado.hexdigest()
+
+def decipherMd5(passwordMd5):
+	for password in passwordsList:
+		if hash_md5(password) == passwordMd5:
+			return("Contraseña encontrada!\n    passwordMd5: "+ str(passwordMd5)+" | Password: "+str(password))
+	return "Contraseña no encontrada :("
+
+print(decipherMd5(input("Introduce el texto hasheado en md5:\n    ")))
