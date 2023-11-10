01b4f208f525420899ac599b2a5824ce25c1d8e8
Javiert54 <83097298+Javiert54@users.noreply.github.com>
2023-11-10 14:41:30 +0000

    Add files via upload

---
+++ sha256Fichero.py
@@ -0,0 +1,13 @@
+import hashlib
+
+def calcular_sha256(archivo):
+    sha256_hash = hashlib.sha256()
+    sha1_hash = hashlib.sha1()
+    with open(archivo,"rb") as f:
+        for byte_block in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(byte_block)
+            sha1_hash.update(byte_block)
+        print("SHA256: ", sha256_hash.hexdigest(), "\nSHA1: ", sha1_hash.hexdigest())
+
+prompt =input("Introduce la ruta del fichero:\n")
+calcular_sha256(prompt)
