f38e7a777901caa86fce42784dce29bcca24d8cb
Javiert54 <jdiazmach@gmail.com>
2023-11-16 16:04:08 +0000

    update

--- sha256Fichero.py
+++ sha256Fichero.py
@@ -1,13 +1,14 @@
 import hashlib
-
 def calcular_sha256(archivo):
-    sha256_hash = hashlib.sha256()
-    sha1_hash = hashlib.sha1()
-    with open(archivo,"rb") as f:
-        for byte_block in iter(lambda: f.read(4096),b""):
-            sha256_hash.update(byte_block)
-            sha1_hash.update(byte_block)
-        print("SHA256: ", sha256_hash.hexdigest(), "\nSHA1: ", sha1_hash.hexdigest())
-
-prompt =input("Introduce la ruta del fichero:\n")
-calcular_sha256(prompt)
+    md5_hash = hashlib.md5() #Instancia de hash md5
+    sha256_hash = hashlib.sha256() #Instancia de hash sha256
+    sha1_hash = hashlib.sha1() # Instancia de hash sha1
+    with open(archivo,"rb") as f: # Abrir archivo en modo binario
+        for byte_block in iter(lambda: f.read(4096),b""): # Interar en el archivo
+            md5_hash.update(byte_block) # Actualizar el hash de md5
+            sha256_hash.update(byte_block) # Actualizar el hash de sha256
+            sha1_hash.update(byte_block) # Actualizar el hash de sha1    
+    #Imprimir los hashes
+    print("SHA256: ", sha256_hash.hexdigest(), "\nSHA1: ", sha1_hash.hexdigest(), "\nmd5: ", md5_hash.hexdigest()) 
+#Ejecutar la función recién declarada pidiendo la ruta del archivo a hashear
+calcular_sha256(input("Introduce la ruta del fichero:\n"))
