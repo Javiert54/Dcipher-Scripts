e35fe49dad212731b631259b77e0c2701f320d4b
Javiert54 <83097298+Javiert54@users.noreply.github.com>
2023-11-24 17:08:26 +0000

    Delete ascii.py

--- ascii.py
+++
@@ -1,7 +0,0 @@
-def descifrar_ascii(codigo_ascii):
-    texto = ''
-    for numero in codigo_ascii.split():
-        texto += chr(int(numero))
-    return texto
-
-print(descifrar_ascii(input("Introduce el texto en código ascii:\n    EJEMPLO: 72 111 108 97 33\n    ")))
